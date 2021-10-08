37d5d305d1b9a2ae3f160d90620457dda24f3600
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-10-08 13:03:40 +0530

    Update posts.page.dart

--- lib/pages/posts/posts.page.dart
+++ lib/pages/posts/posts.page.dart
@@ -23,46 +23,50 @@ class _PostsState extends State<Posts> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20.0,
-                vertical: 10.0,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Sort By'),
-                  PopupMenuButton(
-                    child: Text(
-                      this._postFetchType.toString().split(".")[1],
-                      style: TextStyle(
-                        color: Colors.pink,
-                      ),
-                    ),
-                    itemBuilder: (context) => [
-                      PopupMenuItem(
-                        child: Text("Best of the last 24h"),
-                        value: PostFetchType.BEST,
-                      ),
-                      PopupMenuItem(
-                        child: Text("New Posts"),
-                        value: PostFetchType.NEW,
-                      ),
-                    ],
-                    onSelected: (PostFetchType selectedData) {
-                      setState(() {
-                        this._postFetchType = selectedData;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
+            _buildSorter(),
             PostList(
               postFetchType: this._postFetchType,
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildSorter() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20.0,
+        vertical: 10.0,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('Sort By'),
+          PopupMenuButton(
+            child: Text(
+              this._postFetchType.toString().split(".")[1],
+              style: TextStyle(
+                color: Colors.pink,
+              ),
+            ),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: Text("Best of the last 24h"),
+                value: PostFetchType.BEST,
+              ),
+              PopupMenuItem(
+                child: Text("New Posts"),
+                value: PostFetchType.NEW,
+              ),
+            ],
+            onSelected: (PostFetchType selectedData) {
+              setState(() {
+                this._postFetchType = selectedData;
+              });
+            },
+          ),
+        ],
       ),
     );
   }
