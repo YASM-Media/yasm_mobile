24adda0ddb9143bb97f457b36e6c8e15b07ddb5d
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-10-12 12:27:50 +0530

    Implementation of unliking posts. Closes #40

--- lib/services/like.service.dart
+++ lib/services/like.service.dart
@@ -48,4 +48,44 @@ class LikeService {
       throw NotLoggedInException(message: "User not logged in.");
     }
   }
+
+  Future<void> unlikePost(String postId) async {
+    // Fetch the currently logged in user.
+    FA.User? firebaseUser = this._firebaseAuth.currentUser;
+
+    // Check is the user exists.
+    if (firebaseUser != null) {
+      // Fetching the ID token for authentication.
+      String firebaseAuthToken = await firebaseUser.getIdToken();
+
+      // Preparing the URL for the server request.
+      Uri url = Uri.parse("$endpoint/like-api/unlike");
+
+      // Preparing the headers for the request.
+      Map<String, String> headers = {
+        "Authorization": "Bearer $firebaseAuthToken",
+        "Content-Type": "application/json",
+      };
+
+      // Preparing the body for the request
+      String body = json.encode({"postId": postId});
+
+      // POSTing to the server with new post details.
+      http.Response response = await http.post(
+        url,
+        headers: headers,
+        body: body,
+      );
+
+      // Checking for errors.
+      if (response.statusCode >= 400) {
+        // Decode the response and throw an exception.
+        Map<String, dynamic> body = json.decode(response.body);
+        throw ServerException(message: body["message"]);
+      }
+    } else {
+      // If there is no user logged is using firebase, throw an exception.
+      throw NotLoggedInException(message: "User not logged in.");
+    }
+  }
 }
