b0d81b7117d73bb167323d4c1196fc78bcc1a121
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-10-12 13:23:44 +0530

    Implementation for deleting comment service. Closes #47

--- lib/services/comment.service.dart
+++ lib/services/comment.service.dart
@@ -91,4 +91,43 @@ class CommentService {
     }
   }
 
+  Future<void> deleteComment(DeleteCommentDto deleteCommentDto) async {
+    // Fetch the currently logged in user.
+    FA.User? firebaseUser = this._firebaseAuth.currentUser;
+
+    // Check is the user exists.
+    if (firebaseUser != null) {
+      // Fetching the ID token for authentication.
+      String firebaseAuthToken = await firebaseUser.getIdToken();
+
+      // Preparing the URL for the server request.
+      Uri url = Uri.parse("$endpoint/comments/delete");
+
+      // Preparing the headers for the request.
+      Map<String, String> headers = {
+        "Authorization": "Bearer $firebaseAuthToken",
+        "Content-Type": "application/json",
+      };
+
+      // Preparing the body for the request
+      String body = json.encode(deleteCommentDto.toJson());
+
+      // POSTing to the server with new post details.
+      http.Response response = await http.post(
+        url,
+        headers: headers,
+        body: body,
+      );
+
+      // Checking for errors.
+      if (response.statusCode >= 400) {
+        // Decode the response and throw an exception.
+        Map<String, dynamic> body = json.decode(response.body);
+        throw ServerException(message: body["message"]);
+      }
+    } else {
+      // If there is no user logged is using firebase, throw an exception.
+      throw NotLoggedInException(message: "User not logged in.");
+    }
+  }
 }
