141c17e8d92e32938b514a76a38c7644eacb5812
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-19 09:16:32 +0530

    UI Implementation for displaying logged in user story. Closes #92

--- lib/widgets/stories/stories_list.widget.dart
+++ lib/widgets/stories/stories_list.widget.dart
@@ -5,7 +5,9 @@ import 'package:yasm_mobile/arguments/story.argument.dart';
 import 'package:yasm_mobile/constants/logger.constant.dart';
 import 'package:yasm_mobile/models/user/user.model.dart';
 import 'package:yasm_mobile/pages/stories/story.page.dart';
+import 'package:yasm_mobile/providers/auth/auth.provider.dart';
 import 'package:yasm_mobile/services/stories.service.dart';
+import 'package:yasm_mobile/widgets/stories/logged_in_user_story.widget.dart';
 import 'package:yasm_mobile/widgets/stories/story_item.widget.dart';
 
 class StoriesList extends StatefulWidget {
@@ -43,52 +45,65 @@ class _StoriesListState extends State<StoriesList> {
       scrollDirection: Axis.horizontal,
       child: Container(
         height: MediaQuery.of(context).size.height * 0.1,
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            FutureBuilder(
-              future: this._storiesService.fetchAvailableStories(),
-              builder:
-                  (BuildContext context, AsyncSnapshot<List<User>> snapshot) {
-                if (snapshot.hasError) {
-                  log.e(snapshot.error, snapshot.error, snapshot.stackTrace);
+        child: IntrinsicHeight(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Consumer<AuthProvider>(
+                builder: (BuildContext context, AuthProvider auth, _) =>
+                    LoggedInUserStory(
+                  user: auth.getUser()!,
+                  size: MediaQuery.of(context).size.height * 0.09,
+                ),
+              ),
+              VerticalDivider(
+                thickness: MediaQuery.of(context).size.width * 0.008,
+              ),
+              FutureBuilder(
+                future: this._storiesService.fetchAvailableStories(),
+                builder:
+                    (BuildContext context, AsyncSnapshot<List<User>> snapshot) {
+                  if (snapshot.hasError) {
+                    log.e(snapshot.error, snapshot.error, snapshot.stackTrace);
 
-                  return Text("Something went wrong, please try again later.");
-                }
+                    return Text(
+                        "Something went wrong, please try again later.");
+                  }
 
-                if (snapshot.connectionState == ConnectionState.done) {
-                  this._stories = snapshot.data!;
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    this._stories = snapshot.data!;
 
-                  return Flexible(
-                    fit: FlexFit.loose,
-                    child: ListView.builder(
-                      itemCount: this._stories!.length,
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (BuildContext context, int index) {
-                        User userStory = this._stories![index];
+                    return Flexible(
+                      fit: FlexFit.loose,
+                      child: ListView.builder(
+                        itemCount: this._stories!.length,
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (BuildContext context, int index) {
+                          User userStory = this._stories![index];
 
-                        return StoryItem(
-                          userStory: userStory,
-                          index: index,
-                          size: MediaQuery.of(context).size.height * 0.09,
-                          handleStoryPress: this._handleStoryPress,
-                        );
-                      },
-                    ),
+                          return StoryItem(
+                            userStory: userStory,
+                            index: index,
+                            size: MediaQuery.of(context).size.height * 0.09,
+                            handleStoryPress: this._handleStoryPress,
+                          );
+                        },
+                      ),
+                    );
+                  }
+
+                  return Row(
+                    children: [
+                      CircularProgressIndicator(),
+                    ],
                   );
-                }
-
-                return Row(
-                  children: [
-                    CircularProgressIndicator(),
-                  ],
-                );
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
