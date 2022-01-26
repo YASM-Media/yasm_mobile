ca0e8811c60ad61e568bad70827fdb1fe123e6fb
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-26 08:07:06 +0530

    Update on #99

--- lib/services/chat.service.dart
+++ lib/services/chat.service.dart
@@ -2,4 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Future<String> createChatThread(CreateT)
+
 }
