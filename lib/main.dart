5d8bbb6a3954b0bae9d06167ca67c572a2ca6121
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-26 13:59:27 +0530

    FCM Permissions Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -5,6 +6,7 @@ import 'package:provider/provider.dart';
 import 'package:yasm_mobile/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:yasm_mobile/constants/hive_names.constant.dart';
+import 'package:yasm_mobile/constants/logger.constant.dart';
 import 'package:yasm_mobile/models/image/image.model.dart' as ImageModel;
 import 'package:yasm_mobile/models/like/like.model.dart';
 import 'package:yasm_mobile/models/post/post.model.dart';
@@ -21,6 +23,26 @@ import 'package:yasm_mobile/services/search.service.dart';
 import 'package:yasm_mobile/services/stories.service.dart';
 import 'package:yasm_mobile/services/user.service.dart';
 
+/*
+ * Set up FCM service for server sent notifications.
+ */
+Future<void> setupFCM() async {
+  // Initializing Firebase Notification settings.
+  NotificationSettings settings =
+      await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  // Check for authorization status
+  log.i("FCM Authorization Status: ${settings.authorizationStatus}");
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -35,6 +57,9 @@ Future<void> main() async {
   await Hive.openBox<User>(YASM_USER_BOX);
   await Hive.openBox<List<dynamic>>(YASM_POSTS_BOX);
   await Hive.openBox<List<dynamic>>(YASM_STORIES_BOX);
+
+  await setupFCM();
+
   runApp(Root());
 }
 
