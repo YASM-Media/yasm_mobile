1bf537334373b12aef136eed6825886d0610982b
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-26 08:10:51 +0530

    Service Implementation for creation of a chat thread. Closes #100

--- lib/services/chat.service.dart
+++ lib/services/chat.service.dart
@@ -1,8 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uuid/uuid.dart';
+import 'package:yasm_mobile/dto/chat/create_thread/create_thread.dto.dart';
+import 'package:yasm_mobile/models/chat/chat_thread/chat_thread.model.dart';
 
 class ChatService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final Uuid _uuid = new Uuid();
 
-  // Future<String> createChatThread(CreateT)
+  Future<String> createChatThread(CreateThreadDto createThreadDto) async {
+    ChatThread chatThread = new ChatThread(
+      id: this._uuid.v4(),
+      participants: createThreadDto.participants,
+      messages: [],
+      seen: createThreadDto.participants,
+    );
 
+    await this
+        ._firestore
+        .collection('threads')
+        .doc(chatThread.id)
+        .set(chatThread.toJson());
+
+    return chatThread.id;
+  }
 }
