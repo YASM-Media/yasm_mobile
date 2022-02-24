26a859099ae4297f2835846bd82ae0204fb49f70
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-24 06:48:39 +0530

    UI Implementation for Loading Data List Animation. Closes #140

---
+++ lib/animations/loading_list.animation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class LoadingList extends StatelessWidget {
+  const LoadingList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Lottie.asset(
+      'assets/json/loading-list.json',
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: MediaQuery.of(context).size.height * 0.5,
+      repeat: true,
+    );
+  }
+}
