ddce82fe1d51db7e115b0e0a25789eead3772eed
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-24 08:48:21 +0530

    Refactoring all animations

--- lib/animations/no_data_found.animation.dart
+++ lib/animations/no_data_found.animation.dart
@@ -2,15 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
 class DataNotFound extends StatelessWidget {
-  const DataNotFound({Key? key}) : super(key: key);
+  final String message;
+
+  const DataNotFound({
+    Key? key,
+    required this.message,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Lottie.asset(
-      'assets/json/data_not_found.json',
-      width: MediaQuery.of(context).size.width * 0.9,
-      height: MediaQuery.of(context).size.height * 0.5,
-      repeat: true,
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Lottie.asset(
+            'assets/json/data_not_found.json',
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: MediaQuery.of(context).size.height * 0.5,
+            repeat: true,
+            animate: true,
+            frameRate: FrameRate.max,
+          ),
+          Text(
+            this.message,
+            style: TextStyle(
+              fontSize: MediaQuery.of(context).size.height * 0.03,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
