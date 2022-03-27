6d067971c9762382a0703e1266aeec0dfa2fc0dd
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-03-27 18:39:59 +0530

    Update password_update_tab.widget.dart

--- lib/widgets/user/password_update_tab.widget.dart
+++ lib/widgets/user/password_update_tab.widget.dart
@@ -12,6 +12,7 @@ import 'package:yasm_mobile/utils/display_snackbar.util.dart';
 import 'package:yasm_mobile/utils/validators/value_validator.dart';
 import 'package:yasm_mobile/widgets/common/custom_field.widget.dart';
 import 'package:provider/provider.dart';
+import 'package:yasm_mobile/widgets/common/loading_icon_button.widget.dart';
 
 class PasswordUpdateTab extends StatefulWidget {
   const PasswordUpdateTab({Key? key}) : super(key: key);
@@ -173,53 +174,12 @@ class _PasswordUpdateTabState extends State<PasswordUpdateTab> {
                 textInputType: TextInputType.visiblePassword,
                 obscureText: true,
               ),
-              OfflineBuilder(
-                connectivityBuilder: (
-                  BuildContext context,
-                  ConnectivityResult connectivity,
-                  Widget _,
-                ) {
-                  final bool connected =
-                      connectivity != ConnectivityResult.none;
-                  return ElevatedButton.icon(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        this.loading ? Colors.grey[900]! : Colors.pink,
-                      ),
-                    ),
-                    onPressed: connected
-                        ? !this.loading
-                        ? this._onFormSubmit
-                        : null
-                        : null,
-                    label: Text(
-                      connected
-                          ? !this.loading
-                          ? 'Update Password'
-                          : 'Updating'
-                          : 'You are offline',
-                    ),
-                    icon: connected
-                        ? !this.loading
-                        ? Icon(
-                      Icons.edit,
-                    )
-                        : SizedBox(
-                      height:
-                      MediaQuery.of(context).size.longestSide *
-                          0.025,
-                      width: MediaQuery.of(context).size.longestSide *
-                          0.025,
-                      child: CircularProgressIndicator(
-                        color: Colors.grey,
-                      ),
-                    )
-                        : Icon(
-                      Icons.offline_bolt_outlined,
-                    ),
-                  );
-                },
-                child: SizedBox(),
+              LoadingIconButton(
+                loading: this.loading,
+                iconData: Icons.edit,
+                onPress: this._onFormSubmit,
+                normalText: 'Update Password',
+                loadingText: 'Updating Password',
               ),
             ],
           ),
